37dd5867a2c59ce12d96808df2d55d7aa1bf0620
Edward Palmer <edward.palmer@ucl.ac.uk>
2020-08-17 20:09:14 +0000

    fixed tollerance vd issue

--- R/launch_report.R
+++ R/launch_report.R
@@ -1,6 +1,6 @@
 # This is a gross hack to get the tables into the package namespace
 # and avoid the `cannot change value of locked binding for 'p'` error
-# Should really think about functionalising these? but I don't want to 
+# Should really think about functionalising these? but I don't want to
 # recreate them a bunch of times for the same plots
 # think about more
 p <- NULL
@@ -8,22 +8,21 @@ vo <- NULL
 vd <- NULL
 d <- NULL
 
-#' Launch the shiny version of the report 
-#' 
-#' @param parms_list list version of \code{parms} from 
+#' Launch the shiny version of the report
+#'
+#' @param parms_list list version of \code{parms} from
 #' \code{\link{render_report}}.
-#' 
+#'
 #' @importFrom shiny runApp
-#' 
-#' @return 
+#'
+#' @return
 #' @export
-launch_shiny_report <- function(parms_list) {
-  # hacky hack
-  ctn <<- setup_ctn(parms_list)
-  schema <<- parms_list$schema
+launch_shiny_report <- function(connection = NULL, schema = "decovid_omop") {
 
-  st <<- prepare_tables(ctn, schema)
-  overview <<- prepare_overview(st)
+  ctn <- connection
+
+  st <- prepare_tables(ctn, schema)
+  overview <- prepare_overview(st)
 
   assignInMyNamespace('p', st[["person"]] %>% collect())
   assignInMyNamespace('vo', st[["visit_occurrence"]] %>% collect())
